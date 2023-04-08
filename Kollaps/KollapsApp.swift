b623e4e276d8a5f3a6df4d49bb9aa14a5be9808a
Rasmus Thomsen <oss@cogitri.dev>
2023-04-08 09:46:29 +0200

    Initial view code

---
+++ Kollaps/KollapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  KollapsApp.swift
+//  Kollaps
+//
+//  Created by Rasmus Thomsen on 08.04.23.
+//
+
+import SwiftUI
+
+@main
+struct KollapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
